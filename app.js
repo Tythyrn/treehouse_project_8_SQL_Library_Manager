42314a1acaabbdd8517d0904923076d47c33b7dd
GentlemanMenace <kcrundquist@gmail.com>
2020-10-21 16:50:55 -0500

    Made lots of progress on putting together routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { sequelize } = require('./models');
 
 //brings in all the routes for the app
-const routes = require('./routes/index');
+const routes = require('./routes');
 
 const app = express();
 
@@ -11,11 +11,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //sets up static server
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 //brings in routes
-app.use('/', routes);
+app.use(routes);
 
 //This syncs the database before opening the port to listen on
 sequelize.sync().then(() => {
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,86 @@ const express = require('express');
 const router = express.Router();
 const { Book } = require('../models');
 
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
 /* GET home page. */
-router.get('/', async (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
+  res.redirect('/books');
+})); 
+
+/* GET home page. */
+router.get('/books', asyncHandler(async (req, res) => {
   const books = await Book.findAll();
-  res.render('all_books', { books })
-}); 
+  res.render('index', { books, title: "Books" });
+})); 
+
+/* GET new book form. */
+router.get('/books/new', asyncHandler(async (req, res) => {
+  res.render('new-book', { book: {}, title: "New Book"});
+})); 
+
+/* POST new book form. */
+router.post('/books/new', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books");
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await book.build(req.body);
+      res.render("books/new", { book, errors: error.errors, title: "New Book" })
+    } else {
+      throw error;
+    }
+  }
+})); 
+
+/* GET book details. */
+router.get("/books/:id", asyncHandler(async(req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  res.render("update-book", { book, title: "Update Book" });
+}));
+
+/* POST book details. */
+router.post("/books/:id", asyncHandler(async(req, res) => {
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    if(book) {
+      await book.update(req.body);
+      res.redirect("/books")
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    if(error.name ==  "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      book.id = req.params.id;
+      res.render("/book/" + book.id, { book, errors: error.errors, title: "Edit Book" });
+    }
+  }
+}));
+
+/* POST book details. */
+router.post("/books/:id/delete", asyncHandler(async(req, res) => {
+  book = await Book.findByPk(req.params.id);
+  if(book) {
+    await book.destroy();
+    res.redirect("/books")
+  } else {
+    res.sendStatus(404);
+  }
+}));
 
 //If no routes match then this runs and creates a 404 error
 router.use((req, res, next) => {
